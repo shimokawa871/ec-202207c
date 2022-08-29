fe830ca84b104c210c59ce6f16a0152ae6801042
Ayaka Mizuno <ayaka.mizuno@rakus-partners.co.jp>
2022-08-29 18:03:02 +0900

    8/29

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
-//import Link from 'next/link';
+import Link from 'next/link';
 import ItemList from '../../components/ItemList';
+import Header from '../../components/Header';
+import HeaderCart  from 'components/HeaderCart';
+import HeaderOrder from 'components/HeaderOrder';
 
 
 export default function Page(){
@@ -9,6 +12,9 @@ export default function Page(){
         <Head>
             <title>商品一覧</title>
         </Head>
+        <Header menu={<HeaderCart />}/>
+
+
         <form>
             <p>商品を検索する</p>
             商品名&nbsp;&nbsp;<input
