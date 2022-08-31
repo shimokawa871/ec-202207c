cf084306b610805e6b7d395d705a0e6aae4c3a6c
Ayaka Mizuno <ayaka.mizuno@rakus-partners.co.jp>
2022-08-31 11:58:21 +0900

    module.cssファイル追加

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,38 +1,37 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import ItemList from '../../components/ItemList';
 import Header from '../../components/Header';
-import HeaderCart  from 'components/HeaderCart';
+import HeaderCart from 'components/HeaderCart';
 import HeaderOrder from 'components/HeaderOrder';
 import HeaderLogin from 'components/HeaderLogin';
 import HeaderLogout from 'components/HeaderLogout';
+import Layout from '../../components/layout';
+import styles from '../../components/layout.module.css';
 
-
-export default function Page(){
-    return (
-      <>
-        <Head>
-            <title>ラクラクピザ</title>
-        </Head>
-        <Header
+export default function Page() {
+  return (
+    <Layout>
+      <Head>
+        <title>ラクラクピザ</title>
+      </Head>
+      <Header
         menu1={<HeaderCart />}
         menu2={<HeaderOrder />}
-        menu3 ={<HeaderLogin />}
+        menu3={<HeaderLogin />}
         menu4={<HeaderLogout />}
-        />
-
-
-        <form style={{textAlign:'center',width:'600px',padding:'1%',marginTop:'1%',marginBottom:'3%',marginLeft:'auto',marginRight:'auto',borderRadius:'5%',borderStyle:'dashed',borderColor:'#f0e68c',borderWidth:'3px'}}>
-            <p style={{fontSize:'1.2rem',color:'#ff6347'}}>商品を検索する</p>
-            <div style={{fontWeight:'bold',color:'#ff6347'}}>商品名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input
-            type="text"
-            style={{height:'30px',width:'350px',borderStyle:'solid',borderWidth:'1px',borderColor:'#ff6347',marginBottom:'1%'}}/></div>
-            <br/>
-            <button style={{backgroundColor:'#ff6347',color:'white',fontWeight:'bold',borderRadius:'10%',border:'none',paddingTop:'8px',paddingBottom:'8px',paddingLeft:'15px',paddingRight:'15px'}}>検索</button>&nbsp;&nbsp;
-            <button style={{backgroundColor:'lightgray',color:'white',fontWeight:'bold',borderRadius:'10%',border:'none',paddingTop:'8px',paddingBottom:'8px',paddingLeft:'15px',paddingRight:'15px'}}>クリア</button>
-        </form>
-        <ItemList />
-      </>
-    );
+      />
+      <form className={styles.searchContents}>
+        <p className={styles.search}>商品を検索する</p>
+        <div className={styles.itemTitle}>
+          商品名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="text" className={styles.searchForm} />
+        </div>
+        <br />
+        <button className={styles.searchButton}>検索</button>
+        &nbsp;&nbsp;
+        <button className={styles.clearButton}>クリア</button>
+      </form>
+      <ItemList />
+    </Layout>
+  );
 }
