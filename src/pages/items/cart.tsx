7dc4f9685616691143a9aa9c281f5f6fcd06035a
ikurikijima <110379174+ikurikijima@users.noreply.github.com>
2022-09-07 17:14:54 +0900

    Update cart.tsx

--- src/pages/items/cart.tsx
+++ src/pages/items/cart.tsx
@@ -1,131 +1,54 @@
-import Head from 'next/head';
-import Header from '../../components/Header';
-import HeaderCart from 'components/HeaderCart';
-import HeaderOrder from 'components/HeaderOrder';
-import HeaderLogin from 'components/HeaderLogin';
-import HeaderLogout from 'components/HeaderLogout';
-import useSWR, { useSWRConfig } from 'swr';
-import Image from 'next/image';
-import styles from '../../components/cart.module.css';
+import useSWR from 'swr';
 import Link from 'next/link';
-import OrderItems from 'components/orderItemList';
-import orderConfirmation from "../orderConfirmation"
+import Image from 'next/image';
+import styles from './itemList.module.css';
 
-const fetcher = (resource: any, init: any) =>
-fetch(resource, init).then((res) => res.json());
+export const fetcher = (resource: any, init: any) =>
+  fetch(resource, init).then((res) => res.json());
 
-export default function Cart() {
-  const { data, error } = useSWR(
-    'http://localhost:8000/orderItems',
-    fetcher
-    );
-    const { mutate } = useSWRConfig();
-    if (error) return <div>failed to load</div>;
-    if (!data) return <div>loading...</div>;
+export default function ItemList() {
+  const { data, error } = useSWR('/api/items', fetcher);
 
-    const onClickDelete = (id: number) => {
-      fetch(`http://localhost:8000/orderItems/${id}`, {
-        method: 'delete',
-      });
-      mutate('http://localhost:8000/orderItems');
-    };
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
-    let total: any = 0;
-
-    let  onClickAdd = () =>{
-      location.href = "http://localhost:3000/orderConfirmation";
-    }
+  const sortList = data.sort(function (a: any, b: any) {
+    return a.priceM - b.priceM;
+  });
 
   return (
-    <>
-      <Head>
-        <title>ショッピングカート</title>
-      </Head>
-      <Header
-        menu1={<HeaderCart />}
-        menu2={<HeaderOrder />}
-        menu3={<HeaderLogin />}
-        menu4={<HeaderLogout />}
-      />
-        <div className={styles.searchContents }>
-        <h1></h1>
-        <table>
-        <thead className={styles.th}>
-                    <tr>
-                      <th className={styles.th}>商品名</th>
-                      <th className={styles.th}>サイズ・価格(税抜)・数量</th>
-                      <th className={styles.th}>トッピング・価格（税抜）</th>
-                      <th className={styles.th}>小計</th>
-                    </tr>
-                  </thead>
-
-      {data.map((orderItems: any) => {
+    <div className={styles.itemList}>
+      {sortList.map((item: any) => {
         return (
           <>
-            {/* <div > */}
-              {/* <div> */}
-                {/* <table> */}
-                  <tbody>
-                    <tr>
-                      <td>
-                        <Image
-                          src={orderItems.imagePath}
-                          alt="ピザ"
-                          width={150}
-                          height={100}
-                        />
-                        <p>{`${orderItems.name}`}</p>
-                      </td>
-                      <td>
-                        <span>{`${orderItems.size}　${orderItems.price}円　${orderItems.quantity}個`}</span>
-                      </td>
-                      <td>
-                        <ul>
-                        {orderItems.orderToppingList.map((topping:any) => {
-                          return (
-                          <>
-                            <li className={styles.list}>
-                              <p>{`${topping}`}　{`${orderItems.optionPrice}`}円</p>
-                              </li>
-                          </>
-                          )
-                          })}
-                        </ul>
-                      </td>
-                      <td>{`${orderItems.subTotal}円`} </td>
-                      <td>
-                        <button
-                          onClick={() => onClickDelete(orderItems.id)}
-                        >
-                          削除
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                {/* </table> */}
-              {/* </div> */}
-            {/* </div> */}
+            <div className={styles.item}>
+              <Link href={`/items/${item.id}`}>
+                <a>
+                  <Image
+                    src={item.imagePath}
+                    alt="ピザ"
+                    width={200}
+                    height={125}
+                  />
+                  <div className={styles.itemText}>
+                    <p className={styles.itemName}>{item.name}</p>
+                    <br />
+                    <span className={styles.sizeM}>
+                      &nbsp;M&nbsp;
+                    </span>
+                    &nbsp;{item.priceM}円(税抜)
+                    <br />
+                    <span className={styles.sizeL}>
+                      &nbsp;L&nbsp;
+                    </span>
+                    &nbsp;{item.priceL}円(税抜)
+                  </div>
+                </a>
+              </Link>
+            </div>
           </>
         );
-      }
-      )
-      }
-      </table>
-      {/* {const price = {OrderItems.subTotal:any} */}
-      {data.map((data: any) => {
-          total += data.subTotal}
-          )
-      }
-      <div>{`消費税：${Math.floor(total * 1.08 - total)}円`}</div>
-        <div>{`ご注文金額合計：${Math.floor(total * 1.08)}円（合計）`}</div>
-      {/* } */}
-        <input
-        value={"注文に進む"}
-        className={styles.btn}
-        type="submit"
-        onClick={onClickAdd}
-        />
-        </div>
-    </>
+      })}
+    </div>
   );
 }
